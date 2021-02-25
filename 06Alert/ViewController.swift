c4a74506bf13ceeca371bec960bf8728a1395eba
Haerim <34bestsun@gmail.com>
2021-02-25 19:30:04 +0900

    Initial Commit

---
+++ 06Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  06Alert
+//
+//  Created by 김해림 on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
